78fa1c10598f187687254116512789794c37ff25
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-09-20 14:44:41 +0200

    Add test pop_multi

--- thread_safe_queue_test.c
+++ thread_safe_queue_test.c
@@ -80,3 +80,39 @@ Test(thread_safe_queue, push_nomem) {
     cr_expect_eq(thread_safe_queue_push(&queue, &items[0]), 0);
     cr_expect_eq(queue.len, 1000);
 }
+
+void *pop(void *arg) {
+    struct thread_safe_queue *queue = arg;
+
+    void *popped = NULL;
+    for (size_t i = 0; i < 500; i++)
+        cr_expect_eq(thread_safe_queue_pop(queue, &popped), 0);
+
+    return NULL;
+}
+
+Test(thread_safe_queue, pop_multi) {
+    struct thread_safe_queue queue;
+    struct allocator allocator = {.realloc = realloc, .free = free};
+
+    cr_expect_eq(thread_safe_queue_init(&queue, &allocator), 0);
+
+    // Push
+    int items[1000];
+    for (size_t i = 0; i < 1000; i++) {
+        items[i] = i;
+        cr_expect_eq(thread_safe_queue_push(&queue, &items[i]), 0);
+        cr_expect_eq(queue.len, i + 1);
+    }
+
+    pthread_t thread;
+    pthread_create(&thread, NULL, pop, &queue);
+
+    void *popped = NULL;
+    for (size_t i = 0; i < 500; i++)
+        cr_expect_eq(thread_safe_queue_pop(&queue, &popped), 0);
+
+    pthread_join(thread, NULL);
+
+    cr_expect_eq(queue.len, 0);
+}
