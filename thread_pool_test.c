c6f0127586c8bc9e35f229531d483efde53f2a0c
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-09-20 21:43:10 +0200

    add test

--- thread_pool_test.c
+++ thread_pool_test.c
@@ -10,7 +10,7 @@ Test(thread_pool, init_deinit) {
     thread_pool_deinit(&pool, &allocator);
 }
 
-Test(thread_pool, run_stop) {
+Test(thread_pool, start_stop) {
     struct thread_pool pool;
     struct allocator allocator = {.realloc = realloc, .free = free};
     cr_expect_eq(thread_pool_init(&pool, 4, &allocator), 0);
@@ -18,4 +18,34 @@ Test(thread_pool, run_stop) {
     thread_pool_start(&pool);
     thread_pool_stop(&pool);
     thread_pool_deinit(&pool, &allocator);
+}
+
+void incr(void* arg) {
+    int* count = arg;
+    __atomic_fetch_add(count, 1, __ATOMIC_ACQUIRE);
+}
+
+Test(thread_pool, run_stop) {
+    struct thread_pool pool;
+    struct allocator allocator = {.realloc = realloc, .free = free};
+    cr_expect_eq(thread_pool_init(&pool, 4, &allocator), 0);
+
+    int count = 0;
+    struct work_item work_a = {.arg = &count, .fn = incr};
+    cr_expect_eq(thread_pool_push(&pool, &work_a), 0);
+
+    thread_pool_start(&pool);
+
+    struct work_item work_b = {.arg = &count, .fn = incr};
+    cr_expect_eq(thread_pool_push(&pool, &work_b), 0);
+
+    struct work_item work_c = {.arg = &count, .fn = incr};
+    cr_expect_eq(thread_pool_push(&pool, &work_c), 0);
+
+    while (count != 3) {
+    }
+    thread_pool_stop(&pool);
+    thread_pool_deinit(&pool, &allocator);
+
+    cr_expect_eq(count, 3);
 }
