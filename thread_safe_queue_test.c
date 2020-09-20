6ef5d71cedfb7167c6b254c1ba22a8f359ad23f2
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-09-20 14:27:39 +0200

    add push test

--- thread_safe_queue_test.c
+++ thread_safe_queue_test.c
@@ -18,4 +18,16 @@ Test(thread_safe_queue, pop_empty) {
 
     data_t item;
     cr_expect(thread_safe_queue_pop(&queue, &item) == EINVAL);
+    cr_expect(queue.len == 0);
+}
+
+Test(thread_safe_queue, push) {
+    struct thread_safe_queue queue;
+    struct allocator allocator = {.realloc = realloc, .free = free};
+
+    cr_expect(thread_safe_queue_init(&queue, &allocator) == 0);
+
+    int item = 1;
+    cr_expect(thread_safe_queue_push(&queue, &item) == 0);
+    cr_expect(queue.len == 1);
 }
