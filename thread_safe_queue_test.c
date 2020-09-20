60a10a88251c74b0ab812eda2f4a9ca20cd5b08f
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-09-20 14:35:13 +0200

    Use cr_expect_eq

--- thread_safe_queue_test.c
+++ thread_safe_queue_test.c
@@ -7,27 +7,45 @@ Test(thread_safe_queue, init) {
     struct thread_safe_queue queue;
     struct allocator allocator = {.realloc = realloc, .free = free};
 
-    cr_expect(thread_safe_queue_init(&queue, &allocator) == 0);
+    cr_expect_eq(thread_safe_queue_init(&queue, &allocator), 0);
 }
 
 Test(thread_safe_queue, pop_empty) {
     struct thread_safe_queue queue;
     struct allocator allocator = {.realloc = realloc, .free = free};
 
-    cr_expect(thread_safe_queue_init(&queue, &allocator) == 0);
+    cr_expect_eq(thread_safe_queue_init(&queue, &allocator), 0);
 
     data_t item;
-    cr_expect(thread_safe_queue_pop(&queue, &item) == EINVAL);
-    cr_expect(queue.len == 0);
+    cr_expect_eq(thread_safe_queue_pop(&queue, &item), EINVAL);
+    cr_expect_eq(queue.len, 0);
 }
 
 Test(thread_safe_queue, push) {
     struct thread_safe_queue queue;
     struct allocator allocator = {.realloc = realloc, .free = free};
 
-    cr_expect(thread_safe_queue_init(&queue, &allocator) == 0);
+    cr_expect_eq(thread_safe_queue_init(&queue, &allocator), 0);
 
     int item = 1;
-    cr_expect(thread_safe_queue_push(&queue, &item) == 0);
-    cr_expect(queue.len == 1);
+    cr_expect_eq(thread_safe_queue_push(&queue, &item), 0);
+    cr_expect_eq(queue.len, 1);
+}
+
+Test(thread_safe_queue, push_pop) {
+    struct thread_safe_queue queue;
+    struct allocator allocator = {.realloc = realloc, .free = free};
+
+    cr_expect_eq(thread_safe_queue_init(&queue, &allocator), 0);
+
+    // Push
+    int pushed = 1;
+    { cr_expect_eq(thread_safe_queue_push(&queue, &pushed), 0); }
+
+    // Pop
+    {
+        void *popped = NULL;
+        cr_expect_eq(thread_safe_queue_pop(&queue, &popped), 0);
+        cr_expect_eq(*((int *)popped), 1);
+    }
 }
