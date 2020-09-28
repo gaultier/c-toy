db176ebbc54ac61a434526d55544e279a38fe586
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-09-28 21:14:04 +0200

    [wip]

--- actor_test.c
+++ actor_test.c
@@ -17,7 +17,7 @@ Test(actor, init_deinit) {
 int ping_data = 1;
 int received_ping = 0;
 
-void fn_ping(void* arg) {
+void one_shot_ping(void* arg) {
     PG_ASSERT_NOT_EQ(arg, NULL, "%p");
 
     struct actor* self = arg;
@@ -45,7 +45,30 @@ Test(actor, single_actor) {
     cr_expect_eq(actor_system_init(&actor_system, &allocator), 0);
 
     struct actor actor_ping;
+    cr_expect_eq(actor_init(&actor_ping, one_shot_ping, &actor_system), 0);
+
+    actor_send_message(&actor_ping, actor_ping.id, &ping_data);
+
+    thread_pool_wait_until_finished(&actor_system.pool);
+
+    cr_expect_eq(received_ping, 1);
+
+    actor_system_deinit(&actor_system);
+}
+
+int ping_pong_counter = 0;
+
+Test(actor, ping_pong) {
+    struct allocator allocator = {.realloc = realloc, .free = free};
+
+    struct actor_system actor_system;
+    cr_expect_eq(actor_system_init(&actor_system, &allocator), 0);
+
+    struct actor actor_ping;
     cr_expect_eq(actor_init(&actor_ping, fn_ping, &actor_system), 0);
+
+    struct actor actor_pong;
+    cr_expect_eq(actor_init(&actor_pong, fn_pong, &actor_system), 0);
 
     actor_send_message(&actor_ping, actor_ping.id, &ping_data);
 
